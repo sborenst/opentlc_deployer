12c234a53ebf3e6b548e3b4b44b1b1c4a3b2826b
sborenst <shachar.borenstein@gmail.com>
2016-09-29 17:05:25 +1000

    Added ocp_demo3.3

---
+++ ocp_demo3.3/ansible/scripts/oselab.dns.installer.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+guid=`hostname|cut -f2 -d-|cut -f1 -d.`
+yum -y install bind bind-utils
+systemctl enable named
+systemctl stop named
+
+### firewalld was being a bit problematic
+### Since we turn it off later anyway, I've skipped this step.
+#firewall-cmd --permanent --zone=public --add-service=dns
+#firewall-cmd --reload
+#sleep 100;
+
+infraIP1=`host infranode1-$guid.oslab.opentlc.com ipa.opentlc.com  | grep $guid | awk '{ print $4 }'`
+domain="cloudapps-$guid.oslab.opentlc.com"
+
+echo infraIP 1  is $infraIP1 | tee -a /root/.dns.installer.txt
+echo infraIP 2  is $infraIP2 | tee -a /root/.dns.installer.txt
+echo guid  is $guid | tee -a /root/.dns.installer.txt
+echo domain  is $domain | tee -a /root/.dns.installer.txt
+
+
+
+rm -rf /var/named/zones
+mkdir -p /var/named/zones
+
+echo "\$ORIGIN  .
+\$TTL 1  ;  1 seconds (for testing only)
+${domain} IN SOA master.${domain}.  root.${domain}.  (
+  2011112904  ;  serial
+  60  ;  refresh (1 minute)
+  15  ;  retry (15 seconds)
+  1800  ;  expire (30 minutes)
+  10  ; minimum (10 seconds)
+)
+  NS master.${domain}.
+\$ORIGIN ${domain}.
+test A ${infraIP1}
+* A ${infraIP1}"  >  /var/named/zones/${domain}.db
+
+chgrp named -R /var/named
+chown named -R /var/named/zones
+restorecon -R /var/named
+
+echo "// named.conf
+options {
+  listen-on port 53 { any; };
+  directory \"/var/named\";
+  dump-file \"/var/named/data/cache_dump.db\";
+  statistics-file \"/var/named/data/named_stats.txt\";
+  memstatistics-file \"/var/named/data/named_mem_stats.txt\";
+  allow-query { any; };
+  recursion yes;
+  /* Path to ISC DLV key */
+  bindkeys-file \"/etc/named.iscdlv.key\";
+};
+logging {
+  channel default_debug {
+    file \"data/named.run\";
+    severity dynamic;
+  };
+};
+zone \"${domain}\" IN {
+  type master;
+  file \"zones/${domain}.db\";
+  allow-update { key ${domain} ; } ;
+};" > /etc/named.conf
+
+chown root:named /etc/named.conf
+restorecon /etc/named.conf
+
+systemctl start named
+
+dig @127.0.0.1 test.cloudapps-$guid.oslab.opentlc.com
+
+if [ $? = 0 ]
+then
+  echo "DNS Setup was successful!"
+else
+  echo "DNS Setup failed"
+fi
+
+echo Fully Finished the $0 script  | tee -a /root/.dns.installer.txt
+
+yum install iptables-services -y
+#systemctl stop firewalld
+#systemctl disable firewalld
+systemctl enable iptables
+systemctl start iptables
+
+iptables -I INPUT -p tcp --dport 53 -j ACCEPT
+iptables -I INPUT -p udp --dport 53 -j ACCEPT
+iptables-save > /etc/sysconfig/iptables
---
+++ ocp_demo3.3/init.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+################################################################################
+################################################################################
+# This Script will be used to deploy the OSE_FastAdv and OSE_Demo blueprints
+# This is a temporary method until we do this properly with Ansible playbooks.
+# Steps in the scripts are:
+## Step 0 - Define variables for deployment.
+## Step 1 - Prepare environment and test that all hosts are up
+## Step 2 - Install OpenShift
+## Step 3 - Post-Configure OpenShift (Metrics, Logging)
+## Step 4 - Demo content deployment
+################################################################################
+################################################################################
+
+
+################################################################################
+## Step 0 - Define variables for deployment.
+################################################################################
+#OPENTLC VARS
+export LOGFILE="/root/.oselab.log"
+export USER=$1
+#export USER="shacharb-redhat.com"
+export COURSE=$2;
+#export COURSE="ocp_demo3.3"
+export METRICS="TRUE"
+export LOGGING="TRUE"
+
+
+################################################################################
+## Step 1 - Prepare environemnt and test that all hosts are up
+################################################################################
+echo "---- Step 1 - Prepare environemnt and test that all hosts are up"  2>&1 | tee -a $LOGFILE
+
+
+echo "-- Updating /etc/motd"  2>&1 | tee -a $LOGFILE
+
+cat << EOF > /etc/motd
+###############################################################################
+###############################################################################
+###############################################################################
+Environment Deployment In Progress : ${DATE}
+DO NOT USE THIS ENVIRONMENT AT THIS POINT
+DISCONNECT AND TRY AGAIN 35 MINUTES FROM THE DATE ABOVE
+###############################################################################
+###############################################################################
+If you want, you can check out the status of the installer by using:
+sudo tail -f ${LOGFILE}
+###############################################################################
+
+EOF
+
+echo "ansible-playbook -i /root/.opentlc_deployer/${COURSE}/ansible/files/opentlc.hosts /root/.opentlc_deployer/${COURSE}/ansible/main.yml"   2>&1 | tee -a $LOGFILE
+
+ansible-playbook -i /root/.opentlc_deployer/${COURSE}/ansible/files/opentlc.hosts /root/.opentlc_deployer/${COURSE}/ansible/main.yml   2>&1 | tee -a $LOGFILE
+
+
+## WORKAROUND
+sed -i '/registry/s/^/#/' /etc/exports
+
+
+################################################################################
+## Step 2 - install openshift
+################################################################################
+echo "---- Step 2 - install openshift"  2>&1 | tee -a $LOGFILE
+export HOME="/root"
+echo "ansible-playbook -i /etc/ansible/hosts /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml"  2>&1 | tee -a $LOGFILE
+ansible-playbook -i /etc/ansible/hosts /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml   2>&1 | tee -a $LOGFILE
+
+################################################################################
+## Step 3 - Post-Configure OpenShift (Metrics, Logging)
+################################################################################
+## This will get a done with playbooks later
+echo "---- Step 3 - Post-Configure OpenShift (Metrics, Logging)"  2>&1 | tee -a $LOGFILE
+echo "-- Get the openshift_toolkit repo to deploy METRICS and LOGGING"  2>&1 | tee -a $LOGFILE
+
+scp -r master1.example.com:/root/.kube /root/.kube  2>&1 | tee -a $LOGFILE
+
+if [ $METRICS == "TRUE" ]
+  then
+    echo "Running Ansible playbook for Metrics, logs to ${LOGFILE}.metrics" | tee -a $LOGFILE
+    oc project openshift-infra
+    oc create -f - <<API
+    apiVersion: v1
+    kind: ServiceAccount
+    metadata:
+      name: metrics-deployer
+    secrets:
+    - name: metrics-deployer
+API
+  oadm policy add-role-to-user edit system:serviceaccount:openshift-infra:metrics-deployer
+  oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:openshift-infra:heapster
+  oc secrets new metrics-deployer nothing=/dev/null
+  oc new-app openshift/metrics-deployer-template -p HAWKULAR_METRICS_HOSTNAME=metrics.cloudapps-${GUID}.oslab.opentlc.com -p USE_PERSISTENT_STORAGE=false -p IMAGE_VERSION=3.3.0 -p IMAGE_PREFIX=registry.access.redhat.com/openshift3/
+  oc project default
+
+fi
+
+echo "-- Check pods in the openshift-infra project"  2>&1 | tee -a $LOGFILE
+oc get pods -n openshift-infra  -o wide  2>&1 | tee -a $LOGFILE
+
+echo "-- set the current context to the default project"  2>&1 | tee -a $LOGFILE
+oc project default  2>&1 | tee -a $LOGFILE
+
+if [ $LOGGING == "TRUE" ]
+ then
+   ssh master1.example.com "oc apply -n openshift -f     /usr/share/openshift/examples/infrastructure-templates/enterprise/logging-deployer.yaml"
+
+   oadm new-project logging --node-selector=""
+   oc project logging
+   oc new-app logging-deployer-account-template
+   oadm policy add-cluster-role-to-user oauth-editor        system:serviceaccount:logging:logging-deployer
+   oadm policy add-scc-to-user privileged      system:serviceaccount:logging:aggregated-logging-fluentd
+   oadm policy add-cluster-role-to-user cluster-reader     system:serviceaccount:logging:aggregated-logging-fluentd
+   oc new-app logging-deployer-template --param PUBLIC_MASTER_URL=https://master1-${GUID}.oslab.opentlc.com:8443 --param KIBANA_HOSTNAME=kibana.cloudapps-${GUID}.oslab.opentlc.com --param IMAGE_VERSION=3.3.0 --param IMAGE_PREFIX=registry.access.redhat.com/openshift3/        --param KIBANA_NODESELECTOR='region=infra' --param ES_NODESELECTOR='region=infra' --param MODE=install
+   oc label nodes --all logging-infra-fluentd=true
+   oc label node master1.example.com --overwrite logging-infra-fluentd=false
+   oc project default
+
+ fi
+
+
+
+
+echo "-- Update /etc/motd"  2>&1 | tee -a $LOGFILE
+
+cat << EOF > /etc/motd
+###############################################################################
+Environment Deployment Started      : ${DATE}
+###############################################################################
+###############################################################################
+Environment Deployment Is Completed : `date`
+###############################################################################
+###############################################################################
+
+EOF
+
+
+
+################################################################################
+## Step4 - Demo content deployment
+################################################################################
+echo "---- Step 4 - Demo content deployment"  2>&1 | tee -a $LOGFILE
+
+if [ $DEMO == "TRUE" ]
+  then
+echo "-- Running /root/.opentlc.installer/Demo_Deployment_Script.sh"  2>&1 | tee -a $LOGFILE
+/root/.opentlc.installer/Demo_Deployment_Script.sh 2>&1 | tee -a /root/.Demo.Deployment.log
+echo "-- Finished running /root/.opentlc_deployer/${COURSE}/ansible/files/Demo_Deployment_Script.sh"  2>&1 | tee -a $LOGFILE
+fi
+
+fi
+echo "-- Update /etc/motd"  2>&1 | tee -a $LOGFILE
+
+cat << EOF >> /etc/motd
+###############################################################################
+Demo Materials Deployment Completed : `date`
+###############################################################################
+EOF
+
+echo "-- Update /etc/motd on all nodes"  2>&1 | tee -a $LOGFILE
+ansible all -l masters,nodes,etcd  -m copy -a "src=/etc/motd dest=/etc/motd"
